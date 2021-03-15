4f06df39492120d5d34d179b7b6ed945b3498099
RoyerMohamed <royer.mohamed.pro@gmail.com>
2021-03-15 19:00:46 +0100

    ajoue de lien target blanc & fix scroll reavel

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Portfolio_html_css_js
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,10 @@
-/*===MENU SHOW ===*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu("nav-toggle", "nav-menu")
-
-/*==== ACTIVE AND REMOVE MENU====*/
-const navlink = document.querySelectorAll('.nav_link')
-function linkAction() {
-    // Active link
-    navlink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
-    // remove menu mobile
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
-
-}
-navlink.forEach(n => n.addEventListener('click', linkAction))
-
 /*=====Scroll Home Animation======*/
 
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
     duration: 2000,
-    reset: true
+    reset: false
 })
 sr.reveal('.home_title', {})
 sr.reveal('.button', {delay:200})
